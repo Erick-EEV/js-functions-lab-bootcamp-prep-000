aa6c0cb48484d1452cadc4eb23196d4d56be68be
Erick Villeda <eeverickeev@gmail.com>
2020-07-24 17:20:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-function happyHolidays()
+function happyHolidays(){
+  return "Happy holidays!"
+}
+function happyHolidaysTo(name){
+  return `Happy holidays, ${name}!`
+}
+function happyCustomHolidayTo(holiday,name){
+return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(holiday, days){
+  return `Its ${days} days until ${holiday}!`
+}
